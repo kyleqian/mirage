e813d0f003bc945831e2f895591b4596dc086c6d
Tariq Patanam <t.ahmed.p@gmail.com>
2018-03-11 16:02:25 -0700

    Added demo that connects to websocket

---
+++ unityDemo/Assets/Scripts/DisplayText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class DisplayText : MonoBehaviour {
+
+	private Text userTextField;
+	// Use this for initialization
+	void Start () {
+		SocketClient.Instance.OnTextReceived.AddListener (writeText);
+
+		userTextField = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void writeText(string inputString) {
+		userTextField.text += inputString;
+	}
+}
---
+++ unityDemo/Assets/Scripts/SocketClient.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SocketClient : MonoBehaviour {
+	public class TextReceivedEvent : UnityEvent<string> {
+	}
+
+	public static SocketClient Instance;
+
+	ClientWebSocket webSocket;
+	const int receiveChunkSize = 256;
+
+	public TextReceivedEvent OnTextReceived = new TextReceivedEvent();
+
+	void Awake() {
+		Instance = this;
+	}
+
+	void Start () {
+		Connect ();
+	}
+	
+	void Update () {
+	}
+
+	async Task Receive() {
+		byte[] buffer = new byte[receiveChunkSize];
+
+		while (webSocket.State == WebSocketState.Open) {
+			var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+			if (result.MessageType == WebSocketMessageType.Close)
+			{
+				await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+			}
+			else
+			{
+				OnTextReceived.Invoke (System.Text.Encoding.UTF8.GetString(buffer));
+//				Debug.Log(System.Text.Encoding.UTF8.GetString(buffer));
+			}
+		}
+	}
+
+	async void Connect() {
+		try
+		{
+			webSocket = new ClientWebSocket();
+			await webSocket.ConnectAsync(new Uri("ws://mirage-relay-server.herokuapp.com/"), CancellationToken.None);
+			// await Task.WhenAll(Receive(webSocket), Send(webSocket));
+
+			await Receive();
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine("Exception: {0}", ex);
+		}
+		finally
+		{
+			//if (webSocket != null)
+			//	webSocket.Dispose();
+
+			// Console.WriteLine();
+		}
+	}
+}
