d4bfcd51df31a47d9f8109edecb41ce520f1e91c
Christian Valadez <cvaladez@stanford.edu>
2018-04-08 13:20:19 -0700

    added Stroke logic

---
+++ scribbleNative/scribbleNative/Stroke.swift
@@ -0,0 +1,15 @@
+//
+//  Stroke.swift
+//  scribbleNative
+//
+//  Created by Christian Valadez on 4/8/18.
+//  Copyright © 2018 Christian Valadez. All rights reserved.
+//
+
+import UIKit
+
+struct Stroke {
+    let startPoint: CGPoint
+    let endPoint: CGPoint
+    let color: CGColor
+}
