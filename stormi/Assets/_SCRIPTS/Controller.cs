3a62908264372963d028b5445cccd6d6880f38c7
Kyle Qian <kylecqian@gmail.com>
2018-04-20 17:40:17 -0700

    Actually added Websocket code to Unity side

---
+++ stormi/Assets/_SCRIPTS/Controller.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localEulerAngles = new Vector3(-Mathf.Rad2Deg * SocketHost.instance.pitch, -Mathf.Rad2Deg * SocketHost.instance.yaw, -Mathf.Rad2Deg * SocketHost.instance.roll);
+	}
+}
---
+++ stormi/Assets/_SCRIPTS/SocketHost.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+public class Rotation : WebSocketBehavior
+{
+	protected override void OnMessage(MessageEventArgs e)
+	{
+        //Debug.Log(e.Data);
+        string[] rotationData = e.Data.Split(';');
+        SocketHost.instance.pitch = float.Parse(rotationData[0]);
+        SocketHost.instance.yaw = float.Parse(rotationData[1]);
+        SocketHost.instance.roll = float.Parse(rotationData[2]);
+	}
+}
+
+public class SocketHost : MonoBehaviour
+{
+    public static SocketHost instance;
+    public float pitch;
+    public float yaw;
+    public float roll;
+    WebSocketServer wssv;
+
+	void Awake()
+	{
+        instance = this;
+	}
+
+	void Start()
+    {
+        wssv = new WebSocketServer(9001);
+        wssv.AddWebSocketService<Rotation>("/Rotation");
+        wssv.Start();
+    }
+}
