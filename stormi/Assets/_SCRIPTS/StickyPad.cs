a5ddd39c354a6cf669d1dafc0913c48e48862e40
Tariq Patanam <t.ahmed.p@gmail.com>
2018-06-01 19:22:35 -0700

    Able to instantiate stroke from top to bottom of stroke mirror

--- stormi/Assets/_SCRIPTS/StickyPad.cs
+++ stormi/Assets/_SCRIPTS/StickyPad.cs
@@ -9,39 +9,24 @@ public class StickyPad : MonoBehaviour {
 //	public enum StickyPadState { DELETE, ADD_STICKY, WRITE };
 //	public StickyPadState state;
 	public GameObject NewStickyPrefab;
-	public GameObject whiteboard;
-	public Transform stroke;
+	public GameObject Whiteboard;
+	public StrokeMirror StrokeMirror;
 
 	// source of truth for current text
 	private string currentText;
 	private Text textBox;
+
 	// janky because we're mixing state with events
 	private bool shouldAddSticky = false;
-	private bool shouldDraw = false;
-	private List<List<List<float>>> strokes = new List<List<List<float>>>();
 
 	private System.Random random;
 	private Vector3 whiteboardSize;
-	private const float PHONE_WIDTH = 600f;
-	private const float PHONE_HEIGHT = 400f;
-	private const string TEST_STROKES = "[\n" +
-		"[\n" +
-		"[\n" +
-		"2\n" +
-		"4\n" +
-		"],\n" +
-		"[\n" +
-		"3,\n" +
-		"5,\n" +
-		"]\n" +
-		"]\n" +
-		"]\n";
 
 	void Start () {
         // Get the canvas, then get the text object, then get the text component
         random = new System.Random();
 		textBox = getTextboxFromSticky(this.gameObject);
-		whiteboardSize = whiteboard.GetComponent<Renderer> ().bounds.size;
+		whiteboardSize = Whiteboard.GetComponent<Renderer> ().bounds.size;
 	}
 	
 	// Update is called once per frame
@@ -52,34 +37,10 @@ public class StickyPad : MonoBehaviour {
 			addSticky ();
 			shouldAddSticky = false;
 		}
-
-		if (shouldDraw) {
-			// For each stroke, draw a line through all its points
-//			foreach (List<List<float>> stroke in strokes) {
-//				// Two lists here. First is x points, next is y points
-//
-//			}
-			Transform newStroke = Instantiate(stroke, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
-			newStroke.parent = gameObject.transform;
-			shouldDraw = false;
-		}
-//		switch (state) {
-//		case StickyPadState.DELETE:
-//			
-//			break;
-//		case StickyPadState.ADD_STICKY:
-//			break;
-//		case StickyPadState.WRITE:
-//			break;
-//		default:
-//			break;
-//		}
-//		textBox.text = SocketHost.instance.curText;
 	}
 
 	public void draw(string strokesString) {
-		strokes = deserializeStrokesString (strokesString);
-		shouldDraw = true;
+		StrokeMirror.draw (strokesString);
 	}
 
 	public void addSpace() {
@@ -98,112 +59,6 @@ public class StickyPad : MonoBehaviour {
 		currentText += lastText;
 	}
 
-	private List<List<List<float>>> deserializeHelper(string[] strokesStrings) {
-		int level = 0;
-		List<List<List<float>>> strokes = new List<List<List<float>>> ();
-		List<List<float>> curStroke = new List<List<float>> ();
-
-		for (int i = 0; i < strokesStrings.Length - 1; i++) {
-			string curElem = strokesStrings [i].Trim ().TrimEnd (new char[] { ',' });
-
-			if (level == 0 && curElem == "[") {
-				level += 1;
-			} else if (level == 1) {
-				if (curElem == "[") {
-					level += 1;
-				} else if (curElem == "]") {
-					
-					level -= 1;
-				}
-			} else if (level == 2) {
-				if (curElem == "[") {
-					level += 1;
-				} else if (curElem == "]") {
-					// Reached the end of the current stroke
-					strokes.Add (curStroke);
-					curStroke = new List<List<float>> ();
-
-					level -= 1;
-				}
-			}
-			else if (level == 3) {
-
-				List<float> curList = new List<float> ();
-
-
-				while (curElem != "]") {
-					curList.Add (float.Parse(curElem));
-
-					i += 1;
-					curElem = strokesStrings [i].Trim ().TrimEnd (new char[] { ',' });
-				}
-
-				curStroke.Add (curList);
-				level -= 1;
-			}
-		}
-
-		return strokes;
-
-	}
-
-	private List<List<List<float>>> deserializeStrokesString(string strokesString) {
-		strokesString = strokesString.Trim ().TrimEnd (new char[] { ',' });
-		Debug.Log (strokesString);
-		string[] strokesStringSplit = strokesString.Split (
-						                              new [] { Environment.NewLine },
-						                              StringSplitOptions.None
-					                              );
-
-		// Init an array
-		return deserializeHelper (strokesStringSplit);
-	}
-
-//	private List<List<float[]>> strokeStringToArr(string strokesString) {
-//		string[] strokesStringSplit = strokesString.Split (
-//			                              new [] { Environment.NewLine },
-//			                              StringSplitOptions.None
-//		                              );
-//
-//		// Init an array
-//		List<List<float[]>> strokePoints = new List<List<float[]>> ();
-//
-//		int bracketCount = 0;
-//		float [] curPoint = new float[2];
-//		char[] charsToTrim = { ',' };
-//		// Ignore the first and last elements b/c they are just the opening and closing brackets
-//		for (int i = 1; i < strokesStringSplit.Length - 1; i++) {
-//			string strokeElem = strokesStringSplit [i].Trim ().TrimEnd(charsToTrim);
-//
-//			if (strokeElem == "[") {
-//				bracketCount++;
-//			} else if (strokeElem == "]") {
-//				bracketCount--;
-//			}
-//
-//			if (bracketCount == 2 && strokeElem == "[") {
-//				// Next is a list of x points and a list of y points
-//				string curValue = strokesStringSplit[i].Trim().TrimEnd(charsToTrim); 
-//
-//				// First get the list of x points
-//				while (curValue != "]") {
-//					br
-//				}
-//
-//			} 
-//
-////			else if (bracketCount == 2 && strokeElem == "]") {
-////				// Two lists are over
-////			} else if (bracketCount == 
-////
-////			curPoint [0] = float.Parse(strokesStringSplit[i + 1].TrimEnd(charsToTrim));
-////			curPoint [1] = float.Parse(strokesStringSplit[i + 2]);
-////
-////			strokePoints.Add ((float [])curPoint.Clone());
-////		}
-//
-//		return strokePoints;
-//	}
 
 	private void addSticky() {
 		// Get the current pointing location to board
@@ -218,9 +73,9 @@ public class StickyPad : MonoBehaviour {
 		int randomY = random.Next(-(int)(whiteboardSize.y / 2), (int)(whiteboardSize.y / 2));
 
 		// Get the width and height 
-		Vector3 randPosition = new Vector3(whiteboard.transform.position.x + randomX, 
-			whiteboard.transform.position.y + randomY, 
-			whiteboard.transform.position.z - 0.05f);
+		Vector3 randPosition = new Vector3(Whiteboard.transform.position.x + randomX, 
+			Whiteboard.transform.position.y + randomY, 
+			Whiteboard.transform.position.z - 0.05f);
 
 		// Instantiate new sticky prefab on the board with the text
 		GameObject newSticky = Instantiate(NewStickyPrefab,
@@ -232,8 +87,6 @@ public class StickyPad : MonoBehaviour {
 
 		// Reset the current sticky pad text
 		wipeStickyPad();
-		//			}
-		//		}
 	}
 
 	private int getBeforeLastWordIndex(string words) {
